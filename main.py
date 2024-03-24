1b8aafa3142f4af3c42dc15180d149a93d14737e
kazmifactor <syedmusakazmi99@gmail.com>
2024-03-24 13:03:15 +0500

    Deploying Yolov8 with bytetrack tracker and displaying results

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+from ultralytics import YOLO
 import cv2
 from time import time
 
 
 cap = cv2.VideoCapture("input_video/input_video1.mp4")
+frame_width = int(cap.get(3))
+frame_height = int(cap.get(4))
+source_fps = int(cap.get(cv2.CAP_PROP_FPS))
+print("frame_width: ", frame_width)
+print("frame_height: ", frame_height)
+print("Source FPS: ", source_fps)
 
+model = YOLO("models/yolov8n.pt")
 
 if not cap.isOpened():
     print("Error Opening Video File.")
@@ -13,6 +21,22 @@ while True:
     rect, frame = cap.read()
     if not rect:
         break
+
+    results = model.track(frame, conf=0.1, imgsz=(frame_width, frame_height),
+                          persist=True, classes=[2, 7], tracker="bytetrack.yaml")
+    for result in results:
+        for r in result.boxes.data.tolist():
+            x1, y1, x2, y2, track_id, conf, class_id = r
+            x1 = int(x1)
+            y1 = int(y1)
+            x2 = int(x2)
+            y2 = int(y2)
+            track_id = int(track_id)
+            class_id = int(class_id)
+
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (225, 0, 0), 2)
+            cv2.putText(frame, "car" if class_id == 2 else "truck" if class_id == 7 else None,(x1, y1 - 10),
+                        3, cv2.FONT_HERSHEY_PLAIN, (225, 0, 0), 2)
 
     ctime = time()
     fps = round((1 / (ctime - ptime)), 2)
