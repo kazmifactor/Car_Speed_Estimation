e00263d0f68eda53d55182ca42a9fdf65075bfd2
kazmifactor <syedmusakazmi99@gmail.com>
2024-03-24 12:24:04 +0500

    Getting Video and displaying video

--- main.py
+++ main.py
@@ -1,2 +1,27 @@
-print("sdfghjkl")
-print("checking gfhfg")
+import cv2
+from time import time
+
+
+cap = cv2.VideoCapture("input_video/input_video1.mp4")
+
+
+if not cap.isOpened():
+    print("Error Opening Video File.")
+
+ptime = 0
+while True:
+    rect, frame = cap.read()
+    if not rect:
+        break
+
+    ctime = time()
+    fps = round((1 / (ctime - ptime)), 2)
+    ptime = ctime
+    cv2.putText(frame, f"FPS: {fps}", (30, 40), 4, cv2.FONT_HERSHEY_PLAIN, (0, 225, 0), 2)
+    cv2.namedWindow('Output', cv2.WINDOW_NORMAL)
+    cv2.imshow("Output", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
